f2da4fc95e9c2933b99aa1f64738e3ddf4772df0
osergeiev <os55433@fer.hr>
2026-01-14 14:55:37 +0100

    Test with print outs

---
+++ DbcViewer.TestConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using DbcViewer.Core;
+
+string filePath = Path.Combine(AppContext.BaseDirectory, "example.dbc");
+DbcParser parser = new DbcParser();
+
+var network = parser.Parse(filePath);
+
+foreach (var message in network.Messages)
+{
+    Console.WriteLine($"Message {message.Id} : {message.Name}, Length = {message.Length}, Transmitter = {message.Transmitter}");
+
+    foreach (var signal in message.Signals)
+    {
+        Console.WriteLine($"  Signal: {signal.Name}");
+        Console.WriteLine($"    StartBit: {signal.StartBit}, Length: {signal.Length}");
+        Console.WriteLine($"    Factor: {signal.Factor}, Offset: {signal.Offset}");
+        Console.WriteLine($"    Unit: {(string.IsNullOrEmpty(signal.Unit) ? "(none)" : signal.Unit)}");
+
+        if (signal.Attributes.Count > 0)
+        {
+            Console.WriteLine($"    Attributes:");
+            foreach (var attr in signal.Attributes)
+            {
+                Console.WriteLine($"      {attr.Name} = {attr.Value}");
+            }
+        }
+    }
+}
