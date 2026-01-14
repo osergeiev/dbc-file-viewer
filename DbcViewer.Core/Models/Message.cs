e36ff3044ffb9ae4997a1025cef42c2f8a5087bb
osergeiev <os55433@fer.hr>
2026-01-14 14:55:08 +0100

    Create Models

---
+++ DbcViewer.Core/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbcViewer.Core.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Length { get; set; }
+        public string Transmitter { get; set; } = string.Empty;
+        public List<Signal> Signals { get; } = new List<Signal>();
+    }
+}
---
+++ DbcViewer.Core/Models/Signal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbcViewer.Core.Models
+{
+    public class Signal
+    {
+        public string Name { get; set; } = string.Empty;
+        public int StartBit { get; set; }
+        public int Length { get; set; }
+        public double Factor { get; set; }
+        public double Offset { get; set; }
+        public string Unit { get; set; } = string.Empty;
+        public List<SignalAttribute> Attributes { get; } = new List<SignalAttribute>();
+    }
+}
