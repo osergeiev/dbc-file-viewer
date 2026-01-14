f0fbd35d6f06b1bef5f062d2ba1fff6d77a5b092
osergeiev <os55433@fer.hr>
2026-01-14 15:28:51 +0100

    Fix

--- DbcViewer.Core/IDbcParser.cs
+++ DbcViewer.Core/IDbcParser.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DbcViewer.Core.Models;
+﻿using DbcViewer.Core.Models;
 
 namespace DbcViewer.Core
 {
